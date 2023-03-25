78d12663dc61f469e3f35a7df7883e6d19c6b1cc
Mian Raheel <raheelny@gmail.com>
2023-03-25 00:08:14 -0400

    add fruit search project files

---
+++ script.js
@@ -0,0 +1,40 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruits = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let results = [];
+
+    results = fruits.filter(fruit => fruit.toLowerCase().search(str.toLowerCase()) !== -1);
+
+    showSuggestions(results, str);
+
+	return results;
+}
+
+function searchHandler(e) {
+    let searchValue = input.value;
+    search(searchValue);
+}
+
+function showSuggestions(results, inputVal) {
+    let listItems = "";
+    
+    if (inputVal !== "") {
+        results.forEach(result => {
+            let item = `<li>${result}</li>`;
+            listItems += item;
+        });
+    }
+
+    suggestions.innerHTML = listItems;
+}
+
+function useSuggestion(e) {
+    input.value = e.target.innerHTML;
+    suggestions.innerHTML = "";
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
